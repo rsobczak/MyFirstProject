994fc8b792cbf8bb42065bf9cb0ea2ae5a661e62
Radoslaw Sobczak <r.sobczak@onet.eu>
2020-06-27 00:29:34 +0200

    Initial Commit

---
+++ MyFirstProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by Radoslaw Sobczak on 27/06/2020.
+//  Copyright © 2020 Radoslaw Sobczak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
