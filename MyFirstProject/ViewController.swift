ac50ea95b4aa2b9962d0d861c7fe51825b3c5530
Radoslaw Sobczak <r.sobczak@onet.eu>
2020-07-07 19:25:08 +0200

    dodano: - wpisywanie tylko liczb w Text View - dodanie przycisku oraz akcji do przycisku "Załaduj"

--- MyFirstProject/ViewController.swift
+++ MyFirstProject/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController  // dziedziczenie gdy po obu stronach : stoi TYP
+class ViewController: UIViewController // dziedziczenie gdy po obu stronach : stoi TYP
 { // : dziedziczy
     @IBOutlet weak var textFieldCisnienie: UITextField! // : jest typu ; nazwa obiektu : nazwa klasy
     @IBOutlet weak var textFieldTetno: UITextField!
@@ -17,6 +17,7 @@ class ViewController: UIViewController  // dziedziczenie gdy po obu stronach : s
     @IBOutlet weak var TrybCiemnyLabel: UILabel!
     @IBOutlet weak var SwitchButton: UISwitch!
     @IBOutlet weak var TextViewProbny2: UITextView!
+    @IBOutlet weak var ZaładujButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +29,18 @@ class ViewController: UIViewController  // dziedziczenie gdy po obu stronach : s
         textFieldTetno.delegate = self
         textFieldSaturacja.delegate = self
         textFieldCukier.delegate = self
-
+        
+    //dopisane 06.07.2020 do User default
+    // wyciaganie z przegordki
+    
+    /*
+    let userDefault = UserDefaults.standard
+    TextViewProbny2.text = userDefault.string(forKey: "PodpisPrzegrodki")
+ */
+    }
+    @IBAction func ZaładujButtonAkcja(_ sender: Any) {
+        let userDefault = UserDefaults.standard
+        TextViewProbny2.text = userDefault.string(forKey: "PodpisPrzegrodki")
     }
     
     @IBAction func ZatwierdzButton(_ sender: UIButton) // przycisk jest obiektem tylko przez XX coda zostala przypisana funckcja. I
@@ -62,28 +74,28 @@ class ViewController: UIViewController  // dziedziczenie gdy po obu stronach : s
         if SwitchButton.isOn {
               view.backgroundColor = UIColor.black
               TrybCiemnyLabel.textColor = UIColor.white
-           //TextViewProbny2.textColor = UIColor. // czy to odpowiada za zmiane koloru liter w Text View ?
-
           } else {
               view.backgroundColor = UIColor.white
               TrybCiemnyLabel.textColor = UIColor.black
-            //TextViewProbny2.textColor = UIColor.black   // czy to odpowiada za zmiane koloru liter w Text View ?
           }
-    
     }
-/*
-      if outletSwitch.isOn == true {
-            view.backgroundColor = UIColor.black
-            labelTrybCiemny.textColor = UIColor.white
-         TextViewProbny2.textColor = UIColor.white // czy to odpowiada za zmiane koloru liter w Text View ?
-
-        } else {
-            view.backgroundColor = UIColor.white
-            labelTrybCiemny.textColor = UIColor.black
-          TextViewProbny2.textColor = UIColor.black   // czy to odpowiada za zmiane koloru liter w Text View ?
-        }
-     }
- */
+    
+    /*dopisane dn. 06.07.2020
+    https://www.youtube.com/watch?v=KDloMlCwJnY */
+   // tu wkladamy do przegrodki
+    @IBAction func zapiszPrzycisk(_ sender: Any) {
+        let userDefaults = UserDefaults.standard // standard -> Nazwa "magazynu". W takim magazynie mozna zapisprzegrodki rzegordem ma jakas nalepke. Jak chce wlozyc do magadzynu - to mozwie do jakiej przegordki to wkladam. A jak chce wyjaąć - z jakiej orzegodki chce to wyjac. Nazwa przegodki nadana jest sama. Nazwa magazynu to standard. Metoda "Set" przyjmuje 2 oarametry: 1 to wartosc ktora wkladam do przegordki , a druga wartosc - klucz - tj. nalepka tej przegordki
+        userDefaults.set(TextViewProbny2.text, forKey: "PodpisPrzegrodki") // "podpisPrzegordki" -> nazwa etykiety. SET - to w tym przypadku USTAW -> Co / Gdzie.
+    }
+    
+ // dopisane 07.07.2020 wpisywanie tylko cyfr
+ // delegaty sa przypiete do TextField - na rzecz text fielda wykonuje sie akcja . Moj View Controler jest delegatem dla wszystkich text Fieldow. moze ragować na zdarzenie zmiany tekstu.
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let dopuszczalnyZnak = "+1234567890"
+        let dopuszczlnyZnakZbior = CharacterSet(charactersIn: dopuszczalnyZnak) // dwa teskty: 1 tekst ktos wpisa, a drugi tekst jest wzorcem. I chce sprawdzic czy tekst wpisany jest zgodny ze wzorcem
+        let typowanyZnakowZbior = CharacterSet(charactersIn: string) // [przygotowanie do tego by wywola linijke ponizej. By uzyc metody isSuper set musze miec 2 zbiory by je porownac. Te dwi liniki sa przygotowaniem do wywolania linikji ponizej. czyli te dwi linijki przygotowuja nam ZBIORY.
+        return dopuszczlnyZnakZbior.isSuperset(of: typowanyZnakowZbior ) // metoda isSuperset - wbudowana metoda , ktora na dwoch zbiorach srawdza czy jeden zbior jest supersetem drugiego. Sprwdzmy czy dany zbior jest supersetem dla wpisanych znakow
+    }
 
 }// nawias zamykający klase
 
